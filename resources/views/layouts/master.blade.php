884c069b46a8ef341d9d6b1ce65f12bdb900c2a5
raniasaad <saadrania92@gmail.com>
2017-12-25 07:58:25 -0500

    design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Auth0 Sample</title>
+    <title>SIM Activation</title>
     <link href="https://cdn.auth0.com/styleguide/4.8.10/index.min.css" rel="stylesheet" />
 </head>
 <body>
@@ -9,8 +9,7 @@
     <nav role="navigation" class="navbar navbar-default">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" data-toggle="collapse" data-target="#navbar-collapse" class="navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-          <h1 class="navbar-brand"><a href="/"><span>Auth0</span></a></h1>
+           <h1 class="navbar-brand"><a href="/"><span>Iristel</span></a></h1>
         </div>
         <div id="navbar-collapse" class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
@@ -18,7 +17,9 @@
             @if(!$isLoggedIn)
             <li><a href="{{ route('login') }}" class="signin-button login">Login</a></li>
             @else
+				 <li class="li-docs no-basic"> <h4 style=" margin-top: 20px;">{{ Auth::user()->name }}</h4></li>
             <li class="li-docs no-basic"><a href="{{ route('dump') }}">Dump User</a></li>
+           
             <li><a href="{{ route('logout') }}" class="signin-button login">Logout</a></li>
             @endif
 
