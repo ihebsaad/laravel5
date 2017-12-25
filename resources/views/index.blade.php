784cca8e4d6e53cfda2e8fd54be9d0f47fadb0f2
raniasaad <saadrania92@gmail.com>
2017-12-25 08:38:26 -0500

    fix auth log

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
+ @if (Auth::check())
+                  <?php \Log::debug('logged  2'. Auth::user()->name ); ?>    
+                    @endif
 
-<?php \Log::debug('logged  2'. Auth::user()->name ); ?>
 
 @stop
