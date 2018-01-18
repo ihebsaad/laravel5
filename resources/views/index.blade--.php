d2fa6968034c9a3e41b80a46984f66e6f4484abf
raniasaad <saadrania92@gmail.com>
2018-01-18 04:10:39 -0500

    delete files

--- resources/views/index.blade--.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
- @if (Auth::check())
-                  <?php \Log::debug('logged  2'. Auth::user()->name ); ?>    
-                    @endif
-
-
-@stop
