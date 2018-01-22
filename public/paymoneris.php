15db47b1375aab449ceeab0bdab31f13f32d0ede
haythemsahlia <haithemsahlia@gmail.com>
2018-01-22 10:43:58 +0100

    check moneris response code

--- public/paymoneris.php
+++ public/paymoneris.php
@@ -5,7 +5,7 @@ require './moneris/examples/mpgClasses.php';
 
 if (isset ( $_POST["cvv"]) && isset ( $_POST["creditCard"]) && isset ( $_POST["cardholder"]) && isset ( $_POST["emonth"]) && isset ( $_POST["eyear"])) 
 	{
-		/*try {
+		try {
 			$moneris = Moneris::create(
 			    array(
 			        'api_key' => 'yesguy',
@@ -26,6 +26,21 @@ if (isset ( $_POST["cvv"]) && isset ( $_POST["creditCard"]) && isset ( $_POST["c
             $result = $moneris->purchase($params);
             $transaction = $result->transaction();
 
+            // was it a successful transaction?
+			// any response code greater than 49 is an error code:
+			if ((int) $result->ResponseCode >= 50 || (int) $result->ResponseCode == 0) {
+				// trying to make some sense of this... grouping them as best as I can:
+				switch ($result->ResponseCode) {
+					// ...
+					case '481':
+					case '483':
+						$this->error_code(Moneris_Result::ERROR_DECLINED);
+						break;
+					// ...
+				}
+				return $this->was_successful(false);
+			}
+
             if ($result->was_successful()) {
             	$trnum = $transaction->number();
             	echo $_POST["cvv"].' // '.$_POST["creditCard"].' // '.$_POST["cardholder"].' // '.$_POST["emonth"].' // '.$_POST["eyear"];
@@ -41,23 +56,23 @@ if (isset ( $_POST["cvv"]) && isset ( $_POST["creditCard"]) && isset ( $_POST["c
         } catch (Moneris_Exception $e) {
                 $errors[] = $e->getMessage();
         }
-        */
-$store_id='store1';
+        
+/*$store_id='store1';
 $api_token='yesguy';
 
-/************************* Transactional Variables ****************************/
+
 
 $type='purchase';
 $cust_id='cust id';
 $order_id='iristel-or' . date("dmy-G:i:s");
 $amount='177.00';
 $pan='4242424242424242';
-$expiry_date='0818';
-$crypt='7';
+$expiry_date='1808';
+$crypt='8';
 $dynamic_descriptor='123';
 $status_check = 'false';
 
-/*********************** Transactional Associative Array **********************/
+
 
 $txnArray=array('type'=>$type,
      		    'order_id'=>$order_id,
@@ -69,27 +84,24 @@ $txnArray=array('type'=>$type,
    			    'dynamic_descriptor'=>$dynamic_descriptor
    		       );
 
-/**************************** Transaction Object *****************************/
 
 $mpgTxn = new mpgTransaction($txnArray);
 
-/****************************** Request Object *******************************/
 
 $mpgRequest = new mpgRequest($mpgTxn);
-$mpgRequest->setProcCountryCode("CA"); //"US" for sending transaction to US environment
-$mpgRequest->setTestMode(true); //false or comment out this line for production transactions
+$mpgRequest->setProcCountryCode("CA"); 
+$mpgRequest->setTestMode(true); */
 
-/***************************** HTTPS Post Object *****************************/
+
 
 /* Status Check Example
 $mpgHttpPost  =new mpgHttpsPostStatus($store_id,$api_token,$status_check,$mpgRequest);
 */
-
+/*
 $mpgHttpPost  =new mpgHttpsPost($store_id,$api_token,$mpgRequest);
 
-/******************************* Response ************************************/
 
-$mpgResponse=$mpgHttpPost->getMpgResponse();
+$mpgResponse=$mpgHttpPost->getMpgResponse();*/
 /*
 print("\nCardType = " . $mpgResponse->getCardType());
 print("\nTransAmount = " . $mpgResponse->getTransAmount());
@@ -117,71 +129,4 @@ print_r($msgresp);*/
 	{
 		echo 'empty';
 	}
-	$store_id='store1';
-$api_token='yesguy';
-
-/************************* Transactional Variables ****************************/
-
-$type='purchase';
-$cust_id='cust id';
-$order_id='iristel-or' . date("dmy-G:i:s");
-$amount='177.00';
-$pan='4242424242424242';
-$expiry_date='1808';
-$crypt='8';
-$dynamic_descriptor='123';
-$status_check = 'false';
-
-/*********************** Transactional Associative Array **********************/
-
-$txnArray=array('type'=>$type,
-     		    'order_id'=>$order_id,
-     		    'cust_id'=>$cust_id,
-    		    'amount'=>$amount,
-   			    'pan'=>$pan,
-   			    'expdate'=>$expiry_date,
-   			    'crypt_type'=>$crypt,
-   			    'dynamic_descriptor'=>$dynamic_descriptor
-   		       );
-
-/**************************** Transaction Object *****************************/
-
-$mpgTxn = new mpgTransaction($txnArray);
-
-/****************************** Request Object *******************************/
-
-$mpgRequest = new mpgRequest($mpgTxn);
-$mpgRequest->setProcCountryCode("CA"); //"US" for sending transaction to US environment
-$mpgRequest->setTestMode(true); //false or comment out this line for production transactions
-
-/***************************** HTTPS Post Object *****************************/
-
-
-$mpgHttpPost  =new mpgHttpsPostStatus($store_id,$api_token,$status_check,$mpgRequest);
-
-
-$mpgHttpPost  =new mpgHttpsPost($store_id,$api_token,$mpgRequest);
-
-/******************************* Response ************************************/
-
-$mpgResponse=$mpgHttpPost->getMpgResponse();
-
-print("\nCardType = " . $mpgResponse->getCardType());
-print("\nTransAmount = " . $mpgResponse->getTransAmount());
-print("\nTxnNumber = " . $mpgResponse->getTxnNumber());
-print("\nReceiptId = " . $mpgResponse->getReceiptId());
-print("\nTransType = " . $mpgResponse->getTransType());
-print("\nReferenceNum = " . $mpgResponse->getReferenceNum());
-print("\nResponseCode = " . $mpgResponse->getResponseCode());
-print("\nISO = " . $mpgResponse->getISO());
-print("\nMessage = " . $mpgResponse->getMessage());
-print("\nIsVisaDebit = " . $mpgResponse->getIsVisaDebit());
-print("\nAuthCode = " . $mpgResponse->getAuthCode());
-print("\nComplete = " . $mpgResponse->getComplete());
-print("\nTransDate = " . $mpgResponse->getTransDate());
-print("\nTransTime = " . $mpgResponse->getTransTime());
-print("\nTicket = " . $mpgResponse->getTicket());
-print("\nTimedOut = " . $mpgResponse->getTimedOut());
-print("\nStatusCode = " . $mpgResponse->getStatusCode());
-print("\nStatusMessage = " . $mpgResponse->getStatusMessage());
 ?>
