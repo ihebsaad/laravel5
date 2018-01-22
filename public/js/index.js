b4e10dfb8909be97666facb382f01cc6d871d136
haythemsahlia <haithemsahlia@gmail.com>
2018-01-22 15:21:23 +0100

    error

--- public/js/index.js
+++ public/js/index.js
@@ -671,7 +671,7 @@ var res = $.ajax(settings).done(function (response) {
 
     document.getElementById('transactionid').value=response;
  
-   if ((document.getElementById('transactionid').value == "empty") || (document.getElementById('transactionid').value.startsWith("Array(")))
+   if ((document.getElementById('transactionid').value == "empty") || (document.getElementById('transactionid').value.startsWith("Array")))
   {
     alert ( "Transaction Fail !");
   }
