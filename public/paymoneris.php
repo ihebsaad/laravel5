b457bf41414f1bc6d00c8f3593a17dd8d6f25c35
haythemsahlia <haithemsahlia@gmail.com>
2018-01-24 14:06:21 +0100

    change cvd

--- public/paymoneris.php
+++ public/paymoneris.php
@@ -33,7 +33,7 @@ if (isset ( $_POST["cvv"]) && isset ( $_POST["creditCard"]) && isset ( $_POST["c
 	/************************** CVD Variables *****************************/ 
 	 
 	$cvd_indicator = '1'; 
-	$cvd_value = '198'; 
+	$cvd_value = '099'; 
 	//$cvd_value = $_POST["cvv"]; 
 	 
 	/********************** AVS Associative Array *************************/ 
