37b8dab47958034b9f8b1e55005d689c2f7b0215
ihebsaad <ihebsaad@gmail.com>
2018-01-24 10:48:09 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -67,3 +67,30 @@ Route::post('/pay', function () {
 Route::get('/index2', function () {
     return view('index2');
 });
+
+
+
+
+Route::get('/welcomeemail', function () {
+	
+  Mail::send('emails.email1', [], function ($message) {
+    $message
+      ->from('postmaster@test.enterpriseesolutions.com', 'Houba')
+      ->to('ihebsaad@gmail.com', 'iheb')
+      ->subject('From SparkPost with ❤');
+  });
+  
+   // return redirect()->back();
+});
+
+Route::get('/thankemail', function () {
+	
+  Mail::send('emails.email2', [], function ($message) {
+    $message
+      ->from('postmaster@test.enterpriseesolutions.com', 'Houba')
+      ->to('ihebsaad@gmail.com', 'iheb')
+      ->subject('From SparkPost with ❤');
+  });
+  
+   // return redirect()->back();
+});
