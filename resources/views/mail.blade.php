1975e6e367f8197c35bc4b6d18499a792053c353
raniasaad <saadrania92@gmail.com>
2018-01-25 04:14:47 -0500

    mailing

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,7 +1,7 @@
 <?php 
-           
+   $username=$_GET['reciever']     ;   
 //Mail::send('emails.email1/username?username='.$_GET['reciever'], [], function ($message) {
-Mail::send('email1', ['username'=>$_GET['reciever']], function ($message) {
+Mail::send('email1', ['username'=>$username], function ($message) {
     $message
       ->from('postmaster@test.enterpriseesolutions.com', 'Iristel')
       ->to($_GET['mail'], $_GET['reciever'])
