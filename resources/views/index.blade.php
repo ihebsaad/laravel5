e8bae2641ca83e726e83d83053840e0308f2c597
ihebsaad <ihebsaad@gmail.com>
2018-01-26 18:36:42 +0100

    update style blue border

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
 
 <!DOCTYPE html>
 <html lang="en" >
-
+.contentcontain{ border :5px solid  #006fb9;}
 <head>
   <meta charset="UTF-8">  
   <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
@@ -657,8 +657,8 @@ $('#password, #confirm_password').on('keyup', function () {
 <div class="animate-switch" ng-switch-when="stagePhone">
   <div class="container-triangle"></div>
 <!--<div class="contentcontain">-->
-	<div   class="container center_div"  style="max-width: 850px!important;background-color: white;padding: 20px 20px 20px 20px;box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-    border-radius: 0.4rem!important;" >
+	<div   class="container center_div"  style="max-width: 850px!important;background-color: white;padding: 20px 20px 20px 20px;box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);border :5px solid  #006fb9;
+    ;border-radius: 0.4rem!important;" >
     <div class="form-group">
         <div class="col-sm-12">
         <div class="row">
@@ -1522,7 +1522,6 @@ $(function () {
       Your account is activated successfully
     </h3>
   </div>
-       
  <div class="col-sm-6 col-md-6 col-xs-6 col-lg-6 form-group">
             <button ng-model="test" type="button" ng-click="" class="btn btn-success btn-round" style="float: right;margin-right: 0px;" >OK</button>
         </div>  
