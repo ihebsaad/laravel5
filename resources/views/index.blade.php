f422c90e9977165a735d7ccd5362fa85c4fe150a
haythemsahlia <haithemsahlia@gmail.com>
2018-01-26 16:43:36 +0100

    Success Modal Add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1521,6 +1521,26 @@ $(function () {
 Form : <p>@{{formParams}}</p>-->
   </div>
   <input type="hidden" name="uinfo" id="uinfo" />
+  
+  <!-- Success Modal -->
+<div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">SIM Activation</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Your account is activated successfully
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">OK</button>
+      </div>
+    </div>
+  </div>
+</div>
 </main>
        
  
