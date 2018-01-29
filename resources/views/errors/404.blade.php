b139b9b0988d87c7c30b84a37a92471b54fbbf2e
raniasaad <saadrania92@gmail.com>
2018-01-29 07:49:26 -0500

    page not found

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,10 +5,10 @@
     <div class="col-sm-offset-4 col-sm-4">
         <div class="panel panel-danger">
             <div class="panel-heading">
-                <h3 class="panel-title">Il y a un problème !</h3>
+                <h3 class="panel-title">Ooops !</h3>
             </div>
             <div class="panel-body">
-                <p>Nous sommes désolés mais la page que vous désirez n'existe pas...</p>
+                <p>Page Not Found...</p>
             </div>
         </div>
     </div>
