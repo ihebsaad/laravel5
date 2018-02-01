0133ca55579c491812eff1d5a11eb93d11897f60
raniasaad <saadrania92@gmail.com>
2018-02-01 03:55:23 -0500

    add inout facade

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Input;
+use Illuminate\Support\Facades\Input;
 
 class AdminController extends Controller
 {
