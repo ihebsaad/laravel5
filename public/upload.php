04af22792bc0476666ff3265aef2c1dcd18dfbbd
raniasaad <saadrania92@gmail.com>
2018-02-02 03:56:33 -0500

    alert after upload

--- public/upload.php
+++ public/upload.php
@@ -6,6 +6,24 @@
     else {
         move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
    return 'uploaded';
+   /*Route::get('read-excel',function(){
+    
+        $fileD = fopen('expertphp-product.csv',"r");
+        $column=fgetcsv($fileD);
+        while(!feof($fileD)){
+         $rowData[]=fgetcsv($fileD);
+        }
+        foreach ($rowData as $key => $value) {
+            
+            $inserted_data=array('sim'=>$value[0],
+                                 'pin'=>$value[1],
+                                 'enabled'=>$value[2]
+                            );
+            
+             Sim::create($inserted_data);
+        }
+        print_r($rowData);
+});*/
    }
 
 ?>
