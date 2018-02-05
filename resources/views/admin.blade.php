62d8659f96c4655a32961d8db7627d0c21ba3133
ihebsaad <ihebsaad@gmail.com>
2018-02-05 13:00:20 +0100

    update enable with params

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -84,7 +84,7 @@ echo'
  
   <a href="http://test.enterpriseesolutions.com/activate/admin/enable/{{data.id}}">enable serveur</a>
   <a href="http://127.0.0.1/laravel5/activate/admin/enable/{{data.id}}">enable local</a>
-  <a ng-click="enable(data.enabled);" href="#">enable Ajax Server</a>
+  <a ng-click="enable(data.id);" href="#">enable Ajax Server</a>
 
 </td></tr>
 </table>	
@@ -173,11 +173,11 @@ else{
 <tr><td>PIN : 	</td><td ng-bind="data.pin"></td></tr>
 <tr><td>SIM : 	</td><td ng-bind="data.sim"></td></tr>
 <tr><td>Status :</td><td  ><span   ng-if="data.enabled == 1">Enabled</span> <span   ng-if="data.enabled == 0">Disabled</span> </td></tr>
-<tr ng-if="data.enabled == 0"><td > Enable</td><td>	<input type="hidden" id="idpin" ng-bind="data.id" >
+<tr ng-if="data.enabled == 0"><td > Enable</td><td>	<input type="hidden" id="idpin" ng-bind="data.id" >{{data.id}}</input>
  
   <a href="http://test.enterpriseesolutions.com/activate/admin/enable/{{data.id}}">enable serveur</a>
   <a href="http://127.0.0.1/laravel5/activate/admin/enable/{{data.id}}">enable local</a>
-  <a ng-click="enable(data.enabled);" href="#">enable Ajax Server</a>
+  <a ng-click="enable(data.id);" href="#">enable Ajax Server</a>
 
 </td></tr>
 </table>	
@@ -348,8 +348,36 @@ else{
 <script>
 //@Ran
 
+  /*
+ function enable(id){
+	//  var id =document.getElementById('idpin').value;
+	 // var id =2 ;
+	//  var url = window.location.href+'/enable/2' ;
+	   alert(id);
+ 	  
+	 var setting = {
+  "async": true,
+  "crossDomain": true,
+  "url": "http://test.enterpriseesolutions.com/activate/admin/enable/"+id,
+  "method": "GET",
+  "headers": {
+     'Access-Control-Allow-Origin': '*'
+  },
+  "processData": false 
+ 
+  }
   
+   
+$.ajax(setting).done(function (response) {
+	alert('done' + response);
+});
 
+$.ajax(setting).fail(function (response) {
+	alert('fail'+ response);
+});
+	 
+ }
+ */
  
  /******** Reset password ********/
  
