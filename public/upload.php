7775e3288db7f07dc5cb680404980680eafe003c
raniasaad <saadrania92@gmail.com>
2018-02-05 03:51:53 -0500

    upload csv

--- public/upload.php
+++ public/upload.php
@@ -28,14 +28,52 @@ if ($delimiter != ","){echo 'Incorrect delimeter!'; return false;}
 $enclosure=substr_count($sameline,'"');
 
 if ($enclosure < 14){echo 'Incorrect enclosure!'; return false;}
-     
+     //Get SIM Cards numbers
+	
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://gqnchpomjprsrfglg-mock.stoplight-proxy.io/sim-cards",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "{}"
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+  $obj = json_decode($response);
+  $arraySIMs = array();
+
+foreach($obj->simCards as $sim){
+	echo $sim->iccid;
+	array_push($arraySIMs,$sim->iccid);
+}
+echo 'array result';
+print_r($arraySIMs);
+
+}
+
+
+
 	 $fileD = fopen($csvfile,"r");
         $column=fgetcsv($fileD);
         while(!feof($fileD)){
          $rowData[]=fgetcsv($fileD,',','"');
-		  print_r( ($rowData));
+		//  print_r( ($rowData));
         } 
-		$i=0;$details="";
+		$i=0;$arrayDetails = array();
         foreach ($rowData as $key => $value) {
           
             $i=$i+1;
@@ -52,17 +90,34 @@ if ($enclosure < 14){echo 'Incorrect enclosure!'; return false;}
 			  $ch=substr($ch,strlen($pin)+2);
 			  $status=substr($ch,0,stripos($ch,'"',0));
 			  echo ('status='.$status);
-			  if ( (strlen($sim)==0) && (strlen($pin)>0) ){$details=$details. ' Line '.$i.' is incorrect.';}
+			  
+			  if ( (strlen($sim)==0) && (strlen($pin)>0) )
+			  {$i1=$i+1;$details1= ' Line '. $i1 .' is incorrect.';array_push($arrayDetails,$details1);}
 			  //Case pin exist $ sim not exist
-			  if ( ( (strlen($sim)>0)) && ($pin=='"'))
-				  $details=$details. ' line'.$i.'SIM without PIN' ;
+			  else if ( ( (strlen($sim)>0)) && ($pin=='"'))
+			  {  $i2=$i+1;$details2= ' Line'.$i2 .' SIM without PIN.' ;array_push($arrayDetails,$details2);}
 			  //Case empty line
-			  if ((strlen($sim)==strlen($pin)) && (strlen($sim)  ==strlen($status)) )
-				 $details=$details. ' line '.$i.'empty.';
-			  if($status==""){$status=0;}
+			  else if ((strlen($sim)==strlen($pin)) && (strlen($sim)  ==strlen($status)) )
+			  {$i3=$i+1;$details3= ' Line '. $i3 .' is empty.';array_push($arrayDetails,$details3);}
+			  else if($status==""){$status=0;}
+			  else {
+				if( array_search($sim,$arraySIMs) > -1)
+				{
+					$i4=$i+1;$details4=' Line'. $i4 . ' to be stored.' ;array_push($arrayDetails,$details4 );
+				}
+else {
+	$i5=$i+1;
+	 $details5=' SIM in line '. $i5 .' is not valid. ';
+
+	 array_push($arrayDetails,$details5);
+}				
+			  
+			  }
           
         }
-  echo ('details: '.$details);
+
+  $resultDetails = implode(" ", $arrayDetails);
+  echo ('resultDetails: '.$resultDetails);
 }
 
 ?>
