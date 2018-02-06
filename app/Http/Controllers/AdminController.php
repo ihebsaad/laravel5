8e9cedbb704ec1973fec3fad124a309947df5885
raniasaad <saadrania92@gmail.com>
2018-02-06 09:51:39 -0500

    assign plan

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-
+use App;
 
 class AdminController extends Controller
 {
