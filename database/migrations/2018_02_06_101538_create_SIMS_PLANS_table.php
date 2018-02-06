4667d994901f3bf1e947e455c8a77d86cdd022e0
raniasaad <saadrania92@gmail.com>
2018-02-06 05:19:28 -0500

    database

---
+++ database/migrations/2018_02_06_101538_create_SIMS_PLANS_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSIMSPLANSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('SIM_PLANS', function(Blueprint $table) {
+                $table->increments('id');
+                $table->integer('plan_id')->unsigned();
+                $table->integer('sim_id')->unsigned();
+                $table->foreign('sim_id')->references('id')->on('sims')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+
+                $table->foreign('plan_id')->references('id')->on('plans')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
