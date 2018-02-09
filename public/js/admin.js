f273c75caa27c25ece70508e8bcac7982494b49a
ihebsaad <ihebsaad@gmail.com>
2018-02-09 09:47:08 +0100

    update admin login js

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,14 +6,13 @@ angular.module('formApp', [
 controller('formCtrl', ['$scope', '$http', function($scope, $http) {
 	
   $http.get('https://enterpriseesolutions.com/pins.php').success(function (responsepins) {
-             $scope.DataPins = responsepins ;
+             $scope.DataPins = responsepins ;}
+			 
 	if (document.getElementById('tokeninput').value == "")
 	{ $scope.loggedin = false;}else {$scope.loggedin=true;
- 
           });
-}
+
 //$scope.DataPins ={} ;
-  	
 	
  $scope.stage = "";
     // $scope.formParams = {};
