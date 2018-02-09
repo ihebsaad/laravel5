4c140488d7022c92e53b356e1a0008d87a244d6a
raniasaad <saadrania92@gmail.com>
2018-02-09 10:45:21 -0500

    username

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,6 +28,8 @@
 </head>
 <body>
  <input type="hidden" name="uinfo" id="uinfo" />
+  <div  class='div_session_write2' style="display:none;"> </div>
+ 
 <div class="navbar navbar-inverse bg-inverse" style="border-color: #fff!important;border-radius: 0px!important;margin-bottom: 0px!important;">
 
 <div class="container d-flex justify-content-between">
