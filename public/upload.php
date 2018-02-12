8faffb485714deef561d8fe6d2701578fdce4809
raniasaad <saadrania92@gmail.com>
2018-02-12 04:16:28 -0500

    upload

--- public/upload.php
+++ public/upload.php
@@ -75,13 +75,13 @@ foreach($obj->simCards as $sim){
 
 
 	$fileD = fopen($csvfile,"r");
-      		$i=0;$arrayDetails = array();
+      		$i=1;$arrayDetails = array();
 			echo('i'.$i);
 			echo('$line'.$line);
 			
 			//echo('cond1'.(($line = fgetcsv($fileD)) !== FALSE));
 		//	echo('cond2'.( !feof($fileD)));
-				 if ($i>1){
+			//	 if ($i>1){
         while ((($line = fgetcsv($fileD)) !== FALSE) && ( !feof($fileD))) { 
            
 		
@@ -158,10 +158,10 @@ foreach($obj->simCards as $sim){
 			  
 			       }
 	    	
-			
+			 $i=$i+1;
         } //while
-		 $i=$i+1;
-		}//i>0 
+		
+		//}//i>0 
 $resultDetails = implode(" ", $arrayDetails);
   echo ('resultDetails: '.$resultDetails);
          }
