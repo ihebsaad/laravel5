808c5dd7de54a66d497ce2177fc1fd524a301583
raniasaad <saadrania92@gmail.com>
2018-02-12 10:08:02 -0500

    assign

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -59,12 +59,12 @@ foreach ($arr1 as $key => $value){
   echo (' plan: '.$value);
    for ($i=$start; $i <= $end;$i++) {
  //  echo ' SIM= '.str_pad($i, strlen($start), "0", STR_PAD_LEFT);
- $sim=str_pad($i, strlen($start), "0", STR_PAD_LEFT);
-   echo (' sim: '.$sim);
+ //$sim=str_pad($i, strlen($start), "0", STR_PAD_LEFT);
+   
  //$table1 = App\SIM_PLANS::updateOrCreate( ['planCode' => $value ,'SIM'=>$sim]);
-//  $table1 =  App\SIM_PLANS::firstOrNew(['planCode' => $value ,'SIM'=>$sim]); // your data
+  $table1 =  App\SIM_PLANS::firstOrNew(['planCode' => $value ,'SIM'=>$sim]); // your data
 // make your affectation to the $table1
-//$table1 ->save();
+$table1 ->save();
 }
 }
 	/**/
