c01fbd941559513e534108ba710fc1c9d424cf9b
raniasaad <saadrania92@gmail.com>
2018-02-12 05:58:41 -0500

    test delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -48,17 +48,17 @@ DB::table('SIM_PLANS')->where('SIM', '<=',$end)
                  ->delete();
 	}
 		 public function insertOrUpdate($start,$end,$selectedplans){
-			 //echo('start'.$start);
-		//echo('end'.$end);
-		//echo('selectedplans'.$selectedplans);
+			 echo('start'.$start);
+		echo('end'.$end);
+		echo('selectedplans'.$selectedplans);
 
 
 $arr1 = explode(',',$selectedplans);
-//print_r($arr1);
+print_r($arr1);
 foreach ($arr1 as $key => $value){
   // echo (' plan: '.$value);
    for ($i=$start; $i <= $end;$i++) {
- //  echo ' SIM= '.str_pad($i, strlen($start), "0", STR_PAD_LEFT);
+   echo ' SIM= '.str_pad($i, strlen($start), "0", STR_PAD_LEFT);
  $sim=str_pad($i, strlen($start), "0", STR_PAD_LEFT);
    
  //$table1 = App\SIM_PLANS::updateOrCreate( ['planCode' => $value ,'SIM'=>$sim]);
