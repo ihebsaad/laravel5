88e30ffc0fe2f8aeb41cf124e47f59027154e044
raniasaad <saadrania92@gmail.com>
2018-02-14 03:21:35 -0500

    test3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -69,8 +69,9 @@ DB::table('sims')->where('enabled', '=', 0)
   // convert String to long number
   $startI= Decimal::fromString($start);
   $endI= Decimal::fromString($end);
+  $b=Decimal::fromInteger(10);
   		echo 'startI : '. $startI .'</br> End I : ' .$endI .'</br>';
-  		echo 'start I + 10 : '.  $startI->add(10).'</br>' ;
+  		echo 'start I + 10 : '.  $startI->add($b);  .'</br>' ;
 	/*   for ($i=$startI; $i <= $endI;$i++) {
 //$table1 = App\SIM_PLANS::updateOrCreate( ['planCode' => $value ,'SIM'=>$i]);
  //$table1 ->save();
