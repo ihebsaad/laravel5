d8fccf047a0a9f6cf77a75da3c2b99cd64d11345
raniasaad <saadrania92@gmail.com>
2018-02-15 07:55:11 -0500

    assign range issue

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -103,10 +103,11 @@ DB::table('sims')->where('enabled', '=', 0)
 		foreach ($arr1 as $key => $value){
 		while ($i < $endI)
 		{
-			 $i=$i->add($b);
-			 $i=Decimal::fromString($i);
+			 
 $table1 = App\SIM_PLANS::updateOrCreate( ['planCode' => $value ,'SIM'=>$i]);
  $table1 ->save();
+ $i=$i->add($b);
+			 $i=Decimal::fromString($i);
 echo $i;		echo '</br>' ;
 }
 }
