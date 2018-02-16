16fe56fcf29dd6af09e3322823698218c08c8acf
haythemsahlia <haithemsahlia@gmail.com>
2018-02-16 14:07:05 +0100

    404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,25 +6,11 @@
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%, -50%);
-  
-  /*
-  This doesn't work
-  margin-left: -25%;
-  margin-top: -25%;
-  */
-  
-  width: 40%;
-  height: 50%;
-
-  padding: 20px;  
-  background: #a5a8ab!important;
-  color: white;
-  text-align: center;
-  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+  border-radius: 0.4rem!important;
 }
 </style>
-<div class="notfounddiv">
+<div class="notfounddiv" style="background: #ebebeb!important;">
     <br>
     <h1 style="color:#464646!important;">Page Not Found</h1>
   <p style="color: #a5a8ab!important;">Sorry, but the page you were trying to view does not exist.</p>
