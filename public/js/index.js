9cc4398d835503380b50103b804a9f2c48da864f
iheb <ihebsaad@gmail.com>
2018-05-10 10:37:39 +0100

    test ontario

--- public/js/index.js
+++ public/js/index.js
@@ -1237,7 +1237,8 @@ $('#failsignup').css('display', 'none');
     if (elt.selectedIndex == -1)
 	{Text="";}
 	else {
-    Text=  elt.options[elt.selectedIndex].value;
+    Text=  elt.options[elt.selectedIndex].text;
+	if (Text=='Ontario'){Text='On';}
 	document.getElementById('provinceText').value=Text;
 	document.getElementById('sel2').options[document.getElementById('sel2').selectedIndex].value=-1;$scope.search.city="";
 	document.getElementById('sel3').options[document.getElementById('sel3').selectedIndex].value=-1; $scope.search.areaCode="";
